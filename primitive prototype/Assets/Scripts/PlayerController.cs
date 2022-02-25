8d712d8f660091c46ed27bc139431c4978e1bcf9
AjinRenfire <ajinra4@outlook.com>
2022-02-25 20:34:24 +0530

    added movement(not polished)

--- primitive prototype/Assets/Scripts/PlayerController.cs
+++ primitive prototype/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed;
+
+    private Rigidbody _playerRb; //rb is rigidbody
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _playerRb.AddForce(Vector3.right * Speed * Input.GetAxis("Horizontal"));
+        _playerRb.AddForce(Vector3.forward * Speed * Input.GetAxis("Vertical"));
+
     }
 }
