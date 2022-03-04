556681e3794c5a2bc8f8aee5b251c2cae6a1c6ba
AjinRenfire <ajinra4@outlook.com>
2022-03-04 22:31:10 +0530

    enemies can now attack player

--- primitive prototype/Assets/Scripts/Enemy.cs
+++ primitive prototype/Assets/Scripts/Enemy.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform PlayerPosition;
+
+    private float _speed = 4;
+    private Rigidbody _enemyRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPosition = GameObject.Find("Sphere").transform;
+        _enemyRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = (PlayerPosition.position - transform.position).normalized;
+
+        _enemyRb.AddForce(direction * _speed);
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Bullet"))
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
         }
     }
 }
